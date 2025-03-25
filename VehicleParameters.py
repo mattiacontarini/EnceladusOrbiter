aadcd0f28ba9b24e869f85de33bb209922eb2dcc
mcontarini <mcontarini@student.tudelft.nl>
2025-03-25 13:38:17 +0100

    First commit

---
+++ VehicleParameters.py
@@ -0,0 +1,11 @@
+"""
+File used to define input parameters for the orbiter.
+"""
+
+mass = 2150.0
+
+drag_reference_area = 100.0
+drag_coefficient = 1.2
+
+radiation_pressure_reference_area = 100.0
+radiation_pressure_coefficient = 1.2
