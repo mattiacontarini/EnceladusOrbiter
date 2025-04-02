c4c37f91977f749581e4cd11219cdb935064fc57
mcontarini <mcontarini@student.tudelft.nl>
2025-04-02 11:16:12 +0200

    minor update

--- auxiliary/BenedikterInitialStates.py
+++ auxiliary/BenedikterInitialStates.py
@@ -1,5 +1,9 @@
 import numpy as np
 
+K1_initial_keplerian_state = np.array([0.45362E6, 0.7594E-1, np.deg2rad(0.5517E2), 0, 0, 0])
+K2_initial_keplerian_state = np.array([0.49041E6, 0.7594E-1, np.deg2rad(0.5625E2), 0, 0, 0])
+K3_initial_keplerian_state = np.array([0.47752E6, 0.7594E-1, np.deg2rad(0.5648E2), 0, 0, 0])
+
 K1_initial_cartesian_state = np.array([4.37860747e+05, 9.48716723e+04, -4.47516654e+04, 6.67354927e-01, 7.94830037e+01,  9.86040217e+01])
 K2_initial_cartesian_state = np.array([4.71789124e+05, 1.02222980e+05, -4.82193361e+04, 3.14085658e+00,  7.66580975e+01,  9.50418662e+01])
 K3_initial_cartesian_state = np.array([4.60709175e+05,  9.98223410e+04, -4.70869558e+04,  2.46463058e+00, 7.56765950e+01,  9.76068794e+01])
