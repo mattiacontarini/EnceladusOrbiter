56205092079f3d734ff4a97331f2c85832b5391e
mcontarini <mcontarini@student.tudelft.nl>
2025-04-07 17:33:49 +0200

    load spice kernels

--- GenerateNominalOrbit.py
+++ GenerateNominalOrbit.py
@@ -5,10 +5,11 @@ from auxiliary import BenedikterInitialStates as Benedikter
 
 # Tudat import
 from tudatpy.data import save2txt
+from tudatpy.interface import spice
 
 
 # Define output file
-output_path = "./kernels"
+output_path = "./nominal_orbits"
 
 # Set up orbit propagator object with nominal settings
 UDP = OrbitPropagator.from_config()
@@ -19,8 +20,10 @@ initial_states = [Benedikter.K1_initial_cartesian_state,
                   Benedikter.K3_initial_cartesian_state]
 
 # Load SPICE kernels
-
-
+spice.load_standard_kernels()
+kernels_to_load = ["/Users/mattiacontarini/Documents/Code/Thesis/kernels/de438.bsp",
+                   "/Users/mattiacontarini/Documents/Code/Thesis/kernels/sat427.bsp"]
+spice.load_standard_kernels(kernels_to_load)
 
 for i in range(len(initial_states)):
     initial_state = initial_states[i]
@@ -30,7 +33,7 @@ for i in range(len(initial_states)):
 
     # Save orbit to file
     save2txt(state_history,
-             f"nominal_state_history_{i}.dat",
+             f"nominal_state_history_{i + 1}.dat",
              output_path)
 
 
