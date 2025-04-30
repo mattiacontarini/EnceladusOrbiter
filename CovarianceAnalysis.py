74a9f1a9effcc578fde78b4dfaa763498dc6cb06
mcontarini <mcontarini@student.tudelft.nl>
2025-04-30 11:58:29 +0200

    perform analysis of tuning individual parameters

--- CovarianceAnalysis.py
+++ CovarianceAnalysis.py
@@ -15,10 +15,9 @@ import os
 import sys
 
 
-def tuning_parameters_spectrum_analysis(time_stamp,
-                                        save_simulation_results_flag,
-                                        save_covariance_results_flag,):
-
+def full_parameters_spectrum_analysis(time_stamp,
+                                      save_simulation_results_flag,
+                                      save_covariance_results_flag, ):
     # Add path to compiled version of Tudat
     sys.path.insert(0,
                     "/Users/mattiacontarini/tudat-bundle/tudatpy/src/tudatpy/numerical_simulation/environment_setup/rotation_model/expose_rotation_model.cpp")
@@ -80,14 +79,99 @@ def tuning_parameters_spectrum_analysis(time_stamp,
                     for a_priori_empirical_accelerations_current in a_priori_empirical_accelerations:
                         UDP.a_priori_empirical_accelerations = a_priori_empirical_accelerations_current
                         for a_priori_lander_position_current in a_priori_lander_position:
-                            UDP.a_priori_lander_position_current = a_priori_lander_position_current
+                            UDP.a_priori_lander_position = a_priori_lander_position_current
                             UDP.perform_covariance_analysis(output_path)
+
+
+def perform_tuning_parameters_analysis(time_stamp,
+                                       save_simulation_results_flag,
+                                       save_covariance_results_flag, ):
+    # Add path to the compiled version of Tudat
+    sys.path.insert(0,
+                    "/Users/mattiacontarini/tudat-bundle/cmake-build-debug")
+
+    # Load SPICE kernels for simulation
+    spice.load_standard_kernels()
+    kernels_to_load = [
+        #    "/Users/mattiacontarini/Documents/Code/Thesis/kernels/de438.bsp",
+        #    "/Users/mattiacontarini/Documents/Code/Thesis/kernels/sat427.bsp",
+        "/Users/mattiacontarini/Documents/Code/Thesis/kernels/de440.bsp",
+        "/Users/mattiacontarini/Documents/Code/Thesis/kernels/sat441l.bsp"
+    ]
+    spice.load_standard_kernels(kernels_to_load)
+
+    # Set output path
+    output_folder = "./output/covariance_analysis/tuning_parameters_analysis"
+    output_path = os.path.join(output_folder, time_stamp)
+    os.makedirs(output_path, exist_ok=True)
+
+    # Initialize covariance analysis object
+    UDP = CovarianceAnalysis.from_config()
+
+    # Set flag for saving results
+    UDP.save_simulation_results_flag = save_simulation_results_flag
+    UDP.save_covariance_results_flag = save_covariance_results_flag
+
+    # Set initial states to consider
+    initial_state_indices = [1, 2, 3]
+
+    # Set list of simulation durations to consider
+    simulation_durations = [28.0 * constants.JULIAN_DAY,
+                            60.0 * constants.JULIAN_DAY,
+                            180.0 * constants.JULIAN_DAY,
+                            1.0 * constants.JULIAN_YEAR]
+
+    # Set list of arc durations to consider
+    arc_durations = [1.0 * constants.JULIAN_DAY,
+                     2.0 * constants.JULIAN_DAY,
+                     7.0 * constants.JULIAN_DAY]
+
+    # Set list of values for the Kaula multiplier for a priori constraint on standard deviation
+    kaula_constraint_multipliers = [1e-6, 1e-5, 1e-4, 1e-3]
+
+    # Set list of values for the a priori constraint on the empirical accelerations
+    a_priori_empirical_accelerations = [1e-8, 1e-7, 1e-6, 1e-5, 1e-4, 1e-3]
+
+    # Set list of value for the a priori constraint on the landers position
+    a_priori_lander_position = [1e2, 1e3]
+
+    parameters_to_tune = {
+        "simulation_duration": simulation_durations,
+        "arc_duration": arc_durations,
+        "kaula_constraint_multiplier": kaula_constraint_multipliers,
+        "a_priori_empirical_acceleration": a_priori_empirical_accelerations,
+        "a_priori_lander_position": a_priori_lander_position
+    }
+
+    # Perform covariance analysis varying one parameter singularly
+    for parameter_key in list(parameters_to_tune.keys()):
+        UDP = CovarianceAnalysis.from_config()
+        output_path = os.path.join(output_path, parameter_key)
+        os.makedirs(output_path, exist_ok=True)
+
+        for parameter_value in parameters_to_tune[parameter_key]:
+            parameter_value_index = parameters_to_tune[parameter_key].index(parameter_value)
+            output_path = os.path.join(output_path, f"configuration_{parameter_value_index}")
+            if parameter_key == "simulation_duration":
+                UDP.simulation_duration = parameter_value
+            elif parameter_key == "arc_duration":
+                UDP.arc_duration = parameter_value
+            elif parameter_key == "kaula_constraint_multiplier":
+                UDP.kaula_constraint_multiplier = parameter_value
+            elif parameter_key == "a_priori_empirical_acceleration":
+                UDP.a_priori_empirical_accelerations = parameter_value
+            elif parameter_key == "a_priori_lander_position":
+                UDP.a_priori_lander_position = parameter_value
+            else:
+                raise Exception("Unknown key for parameters to tune.")
+
+            UDP.save_problem_configuration(output_path)
+            UDP.perform_covariance_analysis(output_path)
 
 
 def single_case_analysis(time_stamp,
                          save_simulation_results_flag,
-                         save_covariance_results_flag,):
-
+                         save_covariance_results_flag):
     # Load SPICE kernels for simulation
     spice.load_standard_kernels()
     kernels_to_load = [
@@ -115,7 +199,6 @@ def single_case_analysis(time_stamp,
 
 
 def main():
-
     # Retrieve current time stamp
     time_stamp = datetime.datetime.now().strftime("%Y.%m.%d.%H.%M.%S")
 
@@ -123,13 +206,19 @@ def main():
     save_covariance_results_flag = True
     save_simulation_results_flag = False
 
-    perform_tuning_parameters_spectrum_analysis_flag = False
-    if perform_tuning_parameters_spectrum_analysis_flag:
-        tuning_parameters_spectrum_analysis(time_stamp,
-                                            save_simulation_results_flag,
-                                            save_covariance_results_flag)
+    perform_full_parameters_spectrum_analysis_flag = False
+    if perform_full_parameters_spectrum_analysis_flag:
+        full_parameters_spectrum_analysis(time_stamp,
+                                          save_simulation_results_flag,
+                                          save_covariance_results_flag)
 
-    perform_single_case_analysis_flag = True
+    perform_tuning_paramaters_analysis_flag = True
+    if perform_tuning_paramaters_analysis_flag:
+        perform_tuning_parameters_analysis(time_stamp,
+                                           save_simulation_results_flag,
+                                           save_covariance_results_flag)
+
+    perform_single_case_analysis_flag = False
     if perform_single_case_analysis_flag:
         single_case_analysis(time_stamp,
                              save_simulation_results_flag,
