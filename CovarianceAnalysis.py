e09362e5de3ad6427404bb87c816b9cc42d88937
mcontarini <mcontarini@student.tudelft.nl>
2025-05-07 12:11:13 +0200

    include all landers

--- CovarianceAnalysis.py
+++ CovarianceAnalysis.py
@@ -317,7 +317,7 @@ def single_case_analysis(time_stamp,
     UDP.save_simulation_results_flag = save_simulation_results_flag
     UDP.save_covariance_results_flag = save_covariance_results_flag
 
-    UDP.lander_to_include = []
+    UDP.lander_to_include = ["L1", "L2", "L3", "L4", "L5", "L6", "L7", "L8", "L9"]
 
     # Perform covariance analysis
     UDP.save_problem_configuration(output_path)
