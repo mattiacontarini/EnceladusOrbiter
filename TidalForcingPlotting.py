40bde407c90f343b9f6a242c7167288d709e7a5c
mcontarini <mcontarini@student.tudelft.nl>
2025-05-12 10:24:38 +0200

    Minor updates

--- TidalForcingPlotting.py
+++ TidalForcingPlotting.py
@@ -12,7 +12,7 @@ import matplotlib.lines as mlines
 import os
 
 #######################################################################################################################
-### Generate plots ####################################################################################################
+### Generate figures of merit #########################################################################################
 #######################################################################################################################
 
 degrees_to_consider = [2]
@@ -27,22 +27,31 @@ for degree in degrees_to_consider:
     tidal_forcing_history = np.loadtxt(os.path.join(output_path, "tidal_forcing_history.dat"))
 
     # Compute mean value of tidal forcing
-    mean_tidal_forcing = np.mean(abs(tidal_forcing_history[:, 1]))
+    mean_tidal_forcing = np.mean(tidal_forcing_history[:, 1])
+    np.savetxt(os.path.join(output_path, "mean_tidal_forcing.txt"), [mean_tidal_forcing])
 
+    # Generate plot of tidal forcing history
     fig = plt.figure(figsize=(10, 5))
     ax = fig.add_subplot()
-    ax.plot(tidal_forcing_history[:, 0] / constants.JULIAN_DAY, abs(tidal_forcing_history[:, 1]))
-    ax.hlines(mean_tidal_forcing,
+    ax.plot(tidal_forcing_history[:, 0] / constants.JULIAN_DAY,
+            abs(tidal_forcing_history[:, 1]),
+            color="blue")
+    ax.hlines(abs(mean_tidal_forcing),
               xmin=tidal_forcing_history[0, 0] / constants.JULIAN_DAY,
               xmax=tidal_forcing_history[-1, 0] / constants.JULIAN_DAY,
               linestyles="--",
-              color="black")
+              color="black",
+              linewidth=3)
     ax.set_xlabel(r"$t - t_{0}$  [days]")
-    ax.set_ylabel("Tidal forcing  [-]")
+    ax.set_ylabel(f"Degree {degree} tidal forcing  [-]")
     ax.set_yscale("log")
-    ax.grid(True)
+    ax.grid(True, which="both")
 
-    mean_tidal_forcing_handle = mlines.Line2D([], [], color="black", linestyle="--", label="Mean")
+    mean_tidal_forcing_handle = mlines.Line2D([], [],
+                                              color="black",
+                                              linestyle="--",
+                                              linewidth=3,
+                                              label="Mean")
     ax.legend(handles=[mean_tidal_forcing_handle], loc="upper right")
 
     fig.savefig(os.path.join(output_path, "tidal_forcing.pdf"))
