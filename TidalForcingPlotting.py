f490314fb6c0de03d2a3eb76137b1f9dfba12fb6
mcontarini <mcontarini@student.tudelft.nl>
2025-05-12 11:39:53 +0200

    Plot sine component of tidal forcing

--- TidalForcingPlotting.py
+++ TidalForcingPlotting.py
@@ -27,6 +27,8 @@ for degree in degrees_to_consider:
 
     # Load tidal forcing history
     tidal_forcing_history = np.loadtxt(os.path.join(output_path, "tidal_forcing_history.dat"))
+    tidal_forcing_history_cosine = tidal_forcing_history[:, 1]
+    tidal_forcing_history_sine = tidal_forcing_history[:, 2]
 
     # Load distance history
     distance_history = np.loadtxt(os.path.join(output_path, "distance_history.dat"))
@@ -35,53 +37,66 @@ for degree in degrees_to_consider:
     kepler_elements_history = np.loadtxt(os.path.join(output_path, "kepler_elements_history.dat"))
 
     # Compute mean value of tidal forcing
-    mean_tidal_forcing = np.mean(tidal_forcing_history[:, 1])
-    np.savetxt(os.path.join(output_path, "mean_tidal_forcing.txt"), [mean_tidal_forcing])
+    mean_tidal_forcing = [np.mean(tidal_forcing_history_cosine), np.mean(tidal_forcing_history_sine)]
+    np.savetxt(os.path.join(output_path, "mean_tidal_forcing.txt"), mean_tidal_forcing)
 
     # Compute variation range of tidal forcing
-    variation_range_tidal_forcing = [min(tidal_forcing_history[:, 1]), max(tidal_forcing_history[:, 1])]
+    variation_range_tidal_forcing = np.array([
+        [min(tidal_forcing_history_cosine), max(tidal_forcing_history_cosine)],
+        [min(tidal_forcing_history_sine), max(tidal_forcing_history_sine)],
+    ])
     np.savetxt(os.path.join(output_path, "variation_range_tidal_forcing.txt"), variation_range_tidal_forcing)
 
-    # Plot tidal forcing history
+    # Plot tidal forcing history, cosine component
     fig = plt.figure(figsize=(10, 10), constrained_layout=True)
-    ax1 = fig.add_subplot(3, 1, 1)
-    ax1.plot(tidal_forcing_history[:, 0] / constants.JULIAN_DAY,
-            abs(tidal_forcing_history[:, 1]),
+    ax = fig.add_subplot(4, 1, 1)
+    ax.plot(tidal_forcing_history[:, 0] / constants.JULIAN_DAY,
+            abs(tidal_forcing_history_cosine),
             color="blue")
-    ax1.hlines(abs(mean_tidal_forcing),
+    ax.hlines(abs(mean_tidal_forcing[0]),
               xmin=tidal_forcing_history[0, 0] / constants.JULIAN_DAY,
               xmax=tidal_forcing_history[-1, 0] / constants.JULIAN_DAY,
               linestyles="--",
               color="black",
               linewidth=3)
-    ax1.set_ylabel(f"Degree {degree} tidal forcing  [-]", fontsize=fontsize)
+    ax.set_ylabel(r"$\Delta C_{2,0}$  [-]", fontsize=fontsize)
     mean_tidal_forcing_handle = mlines.Line2D([], [],
                                               color="black",
                                               linestyle="--",
                                               linewidth=3,
                                               label="Mean")
-    ax1.legend(handles=[mean_tidal_forcing_handle], loc="upper right", fontsize=fontsize)
-    ax1.set_yscale("log")
-    ax1.grid(True, which="both")
-    ax1.tick_params(labelsize=fontsize, which="both")
+    ax.legend(handles=[mean_tidal_forcing_handle], loc="upper right", fontsize=fontsize)
+    ax.text(0.0, 2.841e-5, f"Mean:  {abs(mean_tidal_forcing[0])}", fontsize=fontsize)
+    ax.set_yscale("log")
+    ax.set_ylim(top=2.85e-5)
+    ax.grid(True, which="both")
+    ax.tick_params(labelsize=fontsize, which="both")
+    # Plot tidal forcing history, sine component
+    ax = fig.add_subplot(4, 1, 2)
+    ax.plot(tidal_forcing_history[:, 0] / constants.JULIAN_DAY,
+            abs(tidal_forcing_history_sine),
+            color="blue")
+    ax.set_ylabel(r"$\Delta S_{2,0}$  [-]", fontsize=fontsize)
+    ax.grid(True, which="both")
+    ax.tick_params(labelsize=fontsize, which="both")
     # Plot distance history
-    ax2 = fig.add_subplot(3, 1, 2)
-    ax2.plot(distance_history[:, 0] / constants.JULIAN_DAY,
+    ax = fig.add_subplot(4, 1, 3)
+    ax.plot(distance_history[:, 0] / constants.JULIAN_DAY,
              distance_history[:, 1],
              color="blue")
-    ax2.set_ylabel("Distance Enceladus-Saturn  [m]", fontsize=fontsize)
-    ax2.set_yscale("log")
-    ax2.grid(True, which="both")
-    ax2.tick_params(labelsize=fontsize, which="both")
+    ax.set_ylabel("Distance Enceladus-Saturn  [m]", fontsize=fontsize)
+    ax.set_yscale("log")
+    ax.grid(True, which="both")
+    ax.tick_params(labelsize=fontsize, which="both")
     # Plot eccentricity history
-    ax3 = fig.add_subplot(3, 1, 3)
-    ax3.plot(kepler_elements_history[:, 0] / constants.JULIAN_DAY,
+    ax = fig.add_subplot(4, 1, 4)
+    ax.plot(kepler_elements_history[:, 0] / constants.JULIAN_DAY,
              kepler_elements_history[:, 2],
              color="blue")
-    ax3.set_xlabel(r"$t - t_{0}$  [days]", fontsize=fontsize)
-    ax3.set_ylabel("Eccentricity  [-]", fontsize=fontsize)
-    ax3.grid(True, which="both")
-    ax3.tick_params(labelsize=fontsize, which="both")
+    ax.set_xlabel(r"$t - t_{0}$  [days]", fontsize=fontsize)
+    ax.set_ylabel("Eccentricity  [-]", fontsize=fontsize)
+    ax.grid(True, which="both")
+    ax.tick_params(labelsize=fontsize, which="both")
 
     fig.savefig(os.path.join(output_path, "tidal_forcing.pdf"))
     plt.close(fig)
