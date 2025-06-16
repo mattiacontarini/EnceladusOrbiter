e58f4de757ea6d0eb609273b2666056075101b24
mcontarini <mcontarini@student.tudelft.nl>
2025-06-16 11:27:32 +0200

    Update minimum deg gravity in apply_kaula_constraint_a_priori

--- auxiliary/utilities/covariance_analysis_utilities.py
+++ auxiliary/utilities/covariance_analysis_utilities.py
@@ -15,12 +15,12 @@ def get_kaula_constraint(kaula_constraint_multiplier, degree):
     return kaula_constraint_multiplier / degree ** 2
 
 
-def apply_kaula_constraint_a_priori(kaula_constraint_multiplier, max_deg_gravity, indices_cosine_coef, indices_sine_coef, inv_apriori):
+def apply_kaula_constraint_a_priori(kaula_constraint_multiplier, max_deg_gravity, min_deg_gravity, indices_cosine_coef, indices_sine_coef, inv_apriori):
 
     index_cosine_coef = indices_cosine_coef[0]
     index_sine_coef = indices_sine_coef[0]
 
-    for deg in range(2, max_deg_gravity + 1):
+    for deg in range(min_deg_gravity, max_deg_gravity + 1):
         kaula_constraint = get_kaula_constraint(kaula_constraint_multiplier, deg)
         for order in range(deg + 1):
             inv_apriori[index_cosine_coef, index_cosine_coef] = kaula_constraint ** -2
