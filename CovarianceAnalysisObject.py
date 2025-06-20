Correct computation of lander deformation partial

CovarianceAnalysisObject.py
@@ -992,19 +992,23 @@ class CovarianceAnalysis:
             gravitational_parameter_ratio = bodies.get("Saturn").gravitational_parameter / bodies.get("Enceladus").gravitational_parameter
 
             Enceladus_radius = spice.get_average_radius("Enceladus")
+
+            station_name = self.lander_to_include[0]
             station_state_spherical = np.zeros((6,))
-            station_state_spherical[0] = Enceladus_radius + self.lander_coordinates[lander_to_include[0]][0]
-            station_state_spherical[1] = self.lander_coordinates[lander_to_include[0]][1]
-            station_state_spherical[2] = self.lander_coordinates[lander_to_include[0]][2]
+            station_state_spherical[0] = Enceladus_radius + self.lander_coordinates[station_name][0]
+            station_state_spherical[1] = self.lander_coordinates[station_name][1]
+            station_state_spherical[2] = self.lander_coordinates[station_name][2]
             station_position_cartesian = astro.element_conversion.spherical_to_cartesian(station_state_spherical)[:3]
 
             sorted_observation_epochs = CovUtil.retrieve_sorted_observation_epochs(simulated_observations)
-            partials_extended = CovUtil.extend_design_matrix_to_h2_love_number(partials,
+            partials_extended, drL_dh2_dict, dh_dh2_dict = CovUtil.extend_design_matrix_to_h2_love_number(partials,
                                                                                indices_lander,
                                                                                gravitational_parameter_ratio,
                                                                                station_position_cartesian,
+                                                                               station_name,
                                                                                Enceladus_radius,
-                                                                               sorted_observation_epochs)
+                                                                               sorted_observation_epochs,
+                                                                               bodies)
             normalization_terms_extended = CovUtil.get_normalization_terms(partials_extended)
             normalized_partials_extended = CovUtil.normalize_design_matrix(partials_extended, normalization_terms_extended)
             normalized_inv_apriori_extended = CovUtil.normalize_inv_apriori_covariance_matrix(inv_apriori_extended, normalization_terms_extended)
@@ -1407,6 +1411,9 @@ class CovarianceAnalysis:
                 formal_error_radial_love_number_filename = os.path.join(covariance_results_output_path,
                                                                         "formal_error_radial_love_number.dat")
                 np.savetxt(formal_error_radial_love_number_filename, formal_error_radial_love_number)
+
+                save2txt(drL_dh2_dict, "drL_dh2_partials.dat", covariance_results_output_path)
+                save2txt(dh_dh2_dict, "dh_dh2_partials.dat", covariance_results_output_path)
 
             # Save formal error of libration amplitude
             formal_error_libration_amplitude_filename = os.path.join(covariance_results_output_path,

auxiliary/utilities/covariance_analysis_utilities.py
@@ -61,8 +61,10 @@ def extend_design_matrix_to_h2_love_number(design_matrix,
                                            indices_lander_position,
                                            gravitational_parameter_ratio,
                                            station_position,
+                                           station_name,
                                            body_equatorial_radius,
-                                           sorted_observation_epochs):
+                                           sorted_observation_epochs,
+                                           bodies):
 
     nb_observations = design_matrix.shape[0]
     nb_parameters = design_matrix.shape[1]
@@ -73,22 +75,31 @@ def extend_design_matrix_to_h2_love_number(design_matrix,
     station_position_unit_vector = station_position / np.linalg.norm(station_position)
     dh_drL = design_matrix[:, indices_lander_position[0]:indices_lander_position[0] + indices_lander_position[1]]
     dh_dh2 = np.zeros((nb_observations,))
+    drL_dh2_store = dict()
+    dh_dh2_store = dict()
     for i in range(nb_observations):
         epoch = sorted_observation_epochs[i]
-        relative_body_position = spice.get_body_cartesian_position_at_epoch("Saturn",
-                                                                            "Enceladus",
-                                                                            CovAnalysisConfig.global_frame_orientation,
-                                                                            "NONE",
-                                                                            epoch)
+
+        # Compute nominal station position displacement
+        saturn_position_cartesian = bodies.get("Saturn").state_in_base_frame_from_ephemeris(epoch)[:3]
+        enceladus_inertial_to_body_fixed_rotation_matrix = bodies.get("Enceladus").rotation_model.inertial_to_body_fixed_rotation(epoch)
+        relative_body_position_cartesian = np.dot(enceladus_inertial_to_body_fixed_rotation_matrix, saturn_position_cartesian)
         drL_dh2_i = astro.gravitation.calculate_degree_two_basic_tidal_displacement(gravitational_parameter_ratio,
                                                                                     station_position_unit_vector,
-                                                                                    relative_body_position,
+                                                                                    relative_body_position_cartesian,
                                                                                     body_equatorial_radius,
                                                                                     1.0,
                                                                                     0.0)
+
+        drL_dh2_average = CovAnalysisConfig.lander_average_position_deformation[station_name]
+        drL_dh2_i = np.abs(drL_dh2_i - drL_dh2_average)
         dh_dh2[i] = np.dot(dh_drL[i, :], drL_dh2_i)
+
+        drL_dh2_store[epoch] = drL_dh2_i
+        dh_dh2_store[epoch] = dh_dh2[i]
+    
     design_matrix_extended[:, nb_parameters_extended - 1] = dh_dh2
-    return design_matrix_extended
+    return design_matrix_extended, drL_dh2_store, dh_dh2_store
 
 
 def retrieve_sorted_observation_epochs(simulated_observations,):