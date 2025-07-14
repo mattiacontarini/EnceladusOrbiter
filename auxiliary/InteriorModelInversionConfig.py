0cbd087fd9e63aeecf91037cf042382983b3af5b
mcontarini <mcontarini@student.tudelft.nl>
2025-07-14 09:30:36 +0200

    Define nominal central values

--- auxiliary/InteriorModelInversionConfig.py
+++ auxiliary/InteriorModelInversionConfig.py
@@ -8,17 +8,17 @@ import numpy as np
 #######################################################################################################################
 
 # Central value for gravitational Love number of Enceladus
-k2_real = 0.02 # Genova et al. (2024)
-k2_imag = 0.01 # Genova et al. (2024)
+k2_real = 0.011961714142831203
+k2_imag = -6.719558705765207e-07
 k2_real_std = 2.0e-5
 k2_imag_std = 2.1e-5
 
 # Central value for diurnal libration amplitude of Enceladus
-diurnal_libration_amplitude = np.deg2rad(-0.091) # Park et al. (2024)
+diurnal_libration_amplitude = -0.0013205243708144631
 diurnal_libration_amplitude_std = np.rad2deg(2e-6)
 
 # Central value for radial displacement Love number of Enceladus
-h2 = ...
+h2 =  0.029847374006950356
 h2_std = 5e-4
 
 # Define set of observations
@@ -34,14 +34,14 @@ interior_parameters_range = dict(
     rho_shell = [900.0, 1000.0],
     rho_ocean = [1000.0, 1300.0],
     R_ocean = [191.1, 291.0],
-    mu_core = [50.0e9, 70.0e9],
-    mu_shell = [2.5e9, 4.5e9],
+    mu_core = [5.0e9, 70.0e9],
+    mu_shell = [1.0e9, 4.5e9],
 )
 
 # Nominal values for the mass, MoI, radius of Enceladus
 R_Enceladus = 252.1e3 # Porco et al. (2006)
 M_Enceladus = 1.08e20 # Flandes et al. (2023)
-MoI_Enceladus = 0.335 * M_Enceladus * R_Enceladus**2 # Iess et al. (2014)
+MoI_Enceladus = 0.338 * M_Enceladus * R_Enceladus**2 # Iess et al. (2014)
 
 ## Nominal values for the interior parameters of Enceladus
 # Auxiliary base layer (not core)
@@ -56,9 +56,9 @@ nominal_interior_model_base_layer = {
 
 # Core
 nominal_interior_model_core_layer = {
-    "R0": 191.0,
-    "rho0": 2422.0,
-    "mu0": 1e9,
+    "R0": 198.0,
+    "rho0": 2286.6,
+    "mu0": 1.0e9,
     "Ks0": 10.0e9,
     "eta0": 1e20,
     "ocean": 0,
@@ -66,8 +66,8 @@ nominal_interior_model_core_layer = {
 
 # Ocean
 nominal_interior_model_ocean_layer = {
-    "R0": 229.0,
-    "rho0": 1000.0,
+    "R0": 221.0,
+    "rho0": 1063.3,
     "mu0": 3.3e-1,
     "Ks0": 2.2e9,
     "eta0": 1.9e-3,
